9daa0ba03c125d77859184add46fbb76a48832a9
Sabbir-Rahman <sabbirrahman42@iut-dhaka.edu>
2023-12-05 09:53:15 +0600

    lagy load for load when needed, suspense to indicate portion of lay load data, useTransition() to keep beofre value until new value is loaded

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,41 @@
-import { Routes, Route, Outlet, Link } from 'react-router-dom'
-import Home from './components/Home';
+import { Routes, Route, Outlet, Link } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
 import Store from './components/Store';
 import About from './components/About';
 
+const Home = lazy(() => wait(1000).then(() => import('./components/Home')));
 
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<NavWrapper/>}>
-        <Route path='/' element={<Home/>} />
-        <Route path='/store' element={<Store/>} />
-        <Route path='/about' element={<About/>} />
+      <Route path="/" element={<NavWrapper />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/store" element={<Store />} />
+        <Route path="/about" element={<About />} />
       </Route>
     </Routes>
   );
 }
 
 function NavWrapper() {
-  return(
+  return (
     <>
-      <nav style={{ display: "flex", gap: "1rem"}}>
+      <nav style={{ display: 'flex', gap: '1rem' }}>
         <Link to="/">Home</Link>
         <Link to="/store">Store</Link>
         <Link to="/about">About</Link>
       </nav>
-      <Outlet/>
+      <Suspense fallback={<h1>Loading ...</h1>}>
+        <Outlet />
+      </Suspense>
     </>
-  )
+  );
+}
+
+function wait(time) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, time);
+  });
 }
 
 export default App;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,24 +1,38 @@
-import { useState } from "react";
+import { Suspense, useState, useTransition } from 'react';
 
-import AdminData from "./AdminData";
+import AdminData from './AdminData';
 
 const Home = () => {
-  const [isAdmin, setIsAdmin] = useState(false)
+  const [isAdmin, setIsAdmin] = useState(false);
+  const [isPending, startTransiition] = useTransition();
 
   return (
     <>
-        <h1>Home</h1>
-        <button onClick={()=> {
-            import("../sum.js").then(module => {
-                alert(module.sum(2,2))
-            })
-        }}>Add 2 + 2</button>
-        <br />
-        <br />
-        <button onClick={()=> setIsAdmin(prev => !prev)}>Toggle Admin</button>
-        {isAdmin ? <AdminData />: <h2>Not Admin</h2>}
+      <h1>Home</h1>
+      <button
+        onClick={() => {
+          import('../sum.js').then((module) => {
+            alert(module.sum(2, 2));
+          });
+        }}
+      >
+        Add 2 + 2
+      </button>
+      <br />
+      <br />
+      <button
+        onClick={() => {
+          startTransiition(() => setIsAdmin((prev) => !prev));
+        }}
+      >
+        Toggle Admin
+      </button>
+      {isPending && <h2> Loafinh</h2>}
+      <Suspense fallback={<h2>lOADING</h2>}>
+        {isAdmin ? <AdminData /> : <h2>Not Admin</h2>}
+      </Suspense>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
